d93736eb76cd78779c748c9786c10391a537677b
LeonardoTavaresM <leomaltadesa0712@gmail.com>
2024-08-25 16:49:14 -0300

    feat: adding errors page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,23 @@
+import Link from "next/link"
+
+import { ArrowLeft } from "lucide-react"
+
+const NotFoundPage: React.FC = () => {
+  return (
+    <div className="flex h-screen flex-col items-center justify-center gap-1">
+      <p className="text-primary text-5xl font-medium sm:text-8xl">404</p>
+
+      <h1 className="mb-4 text-xl sm:mb-8 sm:text-3xl">
+        Página não encontrada
+      </h1>
+
+      <button className="gap-2">
+        <Link href="/">
+          <ArrowLeft /> Voltar para a página incial
+        </Link>
+      </button>
+    </div>
+  )
+}
+
+export default NotFoundPage
