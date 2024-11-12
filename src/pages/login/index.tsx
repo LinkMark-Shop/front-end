e0a4b3632994b32ce69a367ee65aa234fa4ecee8
leonardoTavaesM <leomaltadesa0712@gmail.com>
2024-11-11 23:58:37 -0300

    feat: adding request logic

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,51 +1,50 @@
-import { useState, ChangeEvent, FormEvent } from "react";
-import { useRouter } from 'next/router';
+import { useState, ChangeEvent, FormEvent } from "react"
+import { useRouter } from "next/router"
 import "./style.css"
 
 interface LoginForm {
-  email: string;
-  password: string;
+  email: string
+  password: string
 }
 
-
-
 const Login = () => {
-
-  const [formData, setFormData] = useState<LoginForm>({ email: '', password: '' });
+  const [formData, setFormData] = useState<LoginForm>({
+    email: "",
+    password: "",
+  })
 
   const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
-    const { name, value } = e.target;
-    setFormData({ ...formData, [name]: value });
-  };
+    const { name, value } = e.target
+    setFormData({ ...formData, [name]: value })
+  }
 
   const handleSubmit = async (e: FormEvent) => {
-    e.preventDefault();
+    e.preventDefault()
     try {
-      const response = await fetch('/api/login', {
-        method: 'POST',
+      const response = await fetch("http://localhost:3001/api/users/login", {
+        method: "POST",
         headers: {
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
         },
         body: JSON.stringify(formData),
-      });
-      const result = await response.json();
-      console.log(result);
+      })
+      await response.json()
+      router.push("/")
     } catch (error) {
-      console.error('Error:', error);
+      console.error("Error:", error)
     }
-  };
+  }
 
-  const [passwordVisible, setPasswordVisible] = useState(false);
+  const [passwordVisible, setPasswordVisible] = useState(false)
 
   const togglePasswordVisibility = () => {
-    setPasswordVisible(!passwordVisible);
-  };
+    setPasswordVisible(!passwordVisible)
+  }
 
-  const router = useRouter();
+  const router = useRouter()
   const handleLoginRedirect = () => {
-    router.push('/register');
-  };
-
+    router.push("/register")
+  }
 
   return (
     <div className="teste">
@@ -53,7 +52,8 @@ const Login = () => {
         <div className="teste3">
           <div className="teste2">
             <h1>Linkmark Shop</h1>
-            <p className="teste5">Entre com suas credenciais para acessar sua conta.
+            <p className="teste5">
+              Entre com suas credenciais para acessar sua conta.
             </p>
           </div>
           <form onSubmit={handleSubmit}>
@@ -65,41 +65,48 @@ const Login = () => {
                 type="text"
                 value={formData.email}
                 onChange={handleInputChange}
-                name="email" />
+                name="email"
+              />
             </div>
             <div className="login-input">
               <div className="teste6">
-                <label className="custom-label">Senha</label><small><strong>Esqueceu sua senha?</strong></small>
+                <label className="custom-label">Senha</label>
+                <small>
+                  <strong>Esqueceu sua senha?</strong>
+                </small>
               </div>
-              <div style={{ position: 'relative', width: '100%' }}>
-                <input className="custom-input"
-                  type={passwordVisible ? 'text' : 'password'}
+              <div style={{ position: "relative", width: "100%" }}>
+                <input
+                  className="custom-input"
+                  type={passwordVisible ? "text" : "password"}
                   name="password"
                   placeholder="Digite sua senha"
                   value={formData.password}
                   onChange={handleInputChange}
                   style={{
-                    paddingRight: '40px'
+                    paddingRight: "40px",
                   }}
                 />
                 <span
                   onClick={togglePasswordVisibility}
                   style={{
-                    position: 'absolute',
-                    right: '15px',
-                    top: '50%',
-                    transform: 'translateY(-50%)',
-                    cursor: 'pointer',
-                    fontSize: '18px',
-                    color: '#333',
+                    position: "absolute",
+                    right: "15px",
+                    top: "50%",
+                    transform: "translateY(-50%)",
+                    cursor: "pointer",
+                    fontSize: "18px",
+                    color: "#333",
                   }}
                 >
-                  {passwordVisible ? '🙈' : '👁️'}
+                  {passwordVisible ? "🙈" : "👁️"}
                 </span>
               </div>
             </div>
             <div>
-              <button type="submit" className="custom-button">Entrar</button>
+              <button type="submit" className="custom-button">
+                Entrar
+              </button>
             </div>
           </form>
           <div className="teste7">
@@ -108,13 +115,19 @@ const Login = () => {
             <div className="linha"></div>
           </div>
           <div className="centra">
-            <p>Não tem nenhuma conta?  <small onClick={handleLoginRedirect} style={{ cursor: 'pointer' }}>Registre-se</small>
+            <p>
+              Não tem nenhuma conta?{" "}
+              <small
+                onClick={handleLoginRedirect}
+                style={{ cursor: "pointer" }}
+              >
+                Registre-se
+              </small>
             </p>
           </div>
         </div>
       </div>
-    </div >
-
+    </div>
   )
 }
 
